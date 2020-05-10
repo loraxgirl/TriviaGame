690614b27ce8eec3f9226d4a15d9c9b3f51f2380
Krista Clarke <kristaclarke82@gmail.com>
2020-05-09 23:19:13 -0700

    loading in the trivia questions and pics. still have a problem when the game runs out of questions

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -4,7 +4,7 @@ $(document).ready(function() {
     // track which question we are on
     var questionCounter = 0;
     // initial time of 15 seconds for each question
-    var time = 15;
+    var time = 20;
     // will keep tally of right guesses for end game
     var correctGuesses = 0;
     //will keep tally of wrong guesses for end game
@@ -12,111 +12,38 @@ $(document).ready(function() {
 
     // question & answer array
     var questions = [
-      {
-	    question: "Who is the main character of Uncharted?",
-	    choices: ["Nathan Drake", "Aloy", "Master Chief", "Mario"],
-	    correctAnswer: "Nathan Drake",
-	    image: "<img src='assets/images/nathanDrake.jpg' class='img-circle shadow'>"
-	  }, 
-	  {
-	    question: "What is the name of the fictional English archaeologist in the game Tomb Raider?",
-	    choices: ["Cortana", "Lara Croft", "Faith", "Kitana"],
-	    correctAnswer: "Lara Croft",
-	    image: "<img src='assets/images/laraCroft.jpg' class='img-circle shadow'>"
-	  }, 
-	  {
-	    question: "Black Ops is the subtitle of which game?",
-	    choices: ["Call of Duty", "Battlefield", "Operation:", "Sims 4"],
-	    correctAnswer: "Call of Duty",
-	    image: "<img src='assets/images/blackOps.jpg' class='img-circle shadow'>"
-	  }, 
-	  {
-	    question: "Pikachu is one of the species of creatures in which series of games?",
-	    choices: ["Digimon", "Space Invaders", "Rick & Morty", "Pokemon"],
-	    correctAnswer: "Pokemon",
-	    image: "<img src='assets/images/pikachu.jpg' class='img-circle shadow'>"
-	  }, 
-	  {
-	    question: "What color is Pac-Man?",
-	    choices: ["Pink", "Blue", "Burnt Orange", "Yellow"],
-	    correctAnswer: "Yellow",
-	    image: "<img src='assets/images/pacMan.png' class='img-circle shadow'>"
-	  },
-	  {
-	    question: "Which character from CD Projekt Red's popular series, The Witcher, is known as The Lady of Space and Time?",
-	    choices: ["Yennefer of Vengerberg", "Ciri", "Triss Merigold", "Keria Metz"],
-	    correctAnswer: "Ciri",
-	    image: "<img src='assets/images/ciri.jpg' class='img-circle shadow'>"
-	  },
-	  {
-	    question: "Spyro is a:",
-	    choices: ["Fighter Pilot", "Dragon", "Hedgehog", "Race Car"],
-	    correctAnswer: "Dragon",
-	    image: "<img src='assets/images/spyro.jpg' class='img-circle shadow'>"
-	  },
-	  {
-	    question: "I'm _____ and this is my favorite store on the Citadel:",
-	    choices: ["Master Chief", "Darth Revan", "Thor", "Commander Shepard"],
-	    correctAnswer: "Commander Shepard",
-	    image: "<img src='assets/images/shep.jpg' class='img-circle shadow'>"
-	  },
-	  {
-	    question: "Who is the protagonist of Nintendo's franchise often set in the Kingdom of Hyrule?",
-	    choices: ["Zelda", "Mario", "Donkey Kong", "Link"],
-	    correctAnswer: "Link",
-	    image: "<img src='assets/images/link.png' class='img-circle shadow'>"
-	  },
-	  {
-	    question: "Which game is most likely to never come out?",
-	    choices: ["Half Life 3", "Half Life 3", "Half Life 3", "Half Life 3"],
-	    correctAnswer: "Half Life 3",
-	    image: "<img src='assets/images/halfLife.jpg' class='img-circle shadow'>"
-	  },
-	  {
-	    question: "Joel and Ellie are two companions in what video game?",
-	    choices: ["Dragon Quest VIII", "Final Fantsay XIII", "The Last of Us", "Dragon Age: Origins"],
-	    correctAnswer: "The Last of Us",
-	    image: "<img src='assets/images/lastOfUs.jpg' class='img-circle shadow'>"
-	  },
-	  {
-	    question: "In which game can you find tears, vigors and robotic patriots?",
-	    choices: ["Bioshock: Infinite", "Modern Warfare", "Wolfenstein", "Civilization IV"],
-	    correctAnswer: "Bioshock: Infinite",
-	    image: "<img src='assets/images/bioshock.jpg' class='img-circle shadow'>"
-	  },
-	  {
-	    question: "Who developed the Mass Effect series? ",
-	    choices: ["Bethesda", "Bioware", "Ubisoft", "2K Games"],
-	    correctAnswer: "Bioware",
-	    image: "<img src='assets/images/bioware.jpg' class='img-circle shadow'>"
-	  },
-	  {
-	    question: "Inhabitants of which fictional town find themselves under the influence of the doctrines of The Order?",
-	    choices: ["Raccoon City", "Silent Hill", "Bright Falls", "Rapture"],
-	    correctAnswer: "Silent Hill",
-	    image: "<img src='assets/images/silentHill.jpg' class='img-circle shadow'>"
-	  },
-	  {
-	    question: "Which of these characters can't jump in any of their games?",
-	    choices: ["Link", "Agent 47", "Pac Man", "James Bond"],
-	    correctAnswer: "Agent 47",
-	    image: "<img src='assets/images/hitman.jpg' class='img-circle shadow'>"
-	  }];
+    
+		{
+			question: "What are the famous dying words of Charles Foster Kane in the movie Citizen Kane?",
+			choices: ["Goodbye", "I Love You", "Rosebud", "Vengeance"],
+			correctAnswer: "Rosebud",
+			image: "<img src='assets/images/citizenkane.jpg'>"
+			}, 
+			
+			{
+			question: "The theme from The Third Man (also called “The Harry Lime Theme” was performed on what instrument?",
+			choices: ["Piano", "Zither", "Guitar", "Flute"],
+			correctAnswer: "Zither",
+			image: "<img src='assets/images/thethirdman.jpg'>"
+			}, 
+			
+			{
+			question: "The line 'Pay no attention to that man behind the curtain.' is from which movie?",
+			choices: ["Casablanca", "Singin in the Rain", "Frozen", "The Wizard of Oz"],
+			correctAnswer: "The Wizard of Oz",
+			image: "<img src='assets/images/thewizardofoz.jpg'>"
+			}
+		 ];
 	  
 
 	// create question contents according to question count
 	function questionContent() {
 		// a for loop would be cool here...
-    	$("#gameScreen").append("<p><strong>" + 
-    		questions[questionCounter].question + 
-    		"</p><p class='choices'>" + 
-    		questions[questionCounter].choices[0] + 
-    		"</p><p class='choices'>" + 
-    		questions[questionCounter].choices[1] + 
-    		"</p><p class='choices'>" + 
-    		questions[questionCounter].choices[2] + 
-    		"</p><p class='choices'>" + 
-    		questions[questionCounter].choices[3] + 
+    	$("#gameScreen").append("<p><strong>" + questions[questionCounter].question + 
+    		"</p><p class='choices'>" + questions[questionCounter].choices[0] + 
+    		"</p><p class='choices'>" + questions[questionCounter].choices[1] + 
+    		"</p><p class='choices'>" + questions[questionCounter].choices[2] + 
+    		"</p><p class='choices'>" + questions[questionCounter].choices[3] + 
     		"</strong></p>");
 	}
 
@@ -126,9 +53,7 @@ $(document).ready(function() {
 		correctGuesses++;
 		var correctAnswer = questions[questionCounter].correctAnswer;
 		$("#gameScreen").append("<p>The answer was <span class='answer'>" + 
-			correctAnswer + 
-			"</span></p>" + 
-			questions[questionCounter].image);
+			correctAnswer + "</span></p>" + questions[questionCounter].image);
 		setTimeout(nextQuestion, 4000);
 		questionCounter++;
 	}
@@ -139,9 +64,7 @@ $(document).ready(function() {
 		incorrectGuesses++;
 		var correctAnswer = questions[questionCounter].correctAnswer;
 		$("#gameScreen").append("<p>The answer was <span class='answer'>" + 
-			correctAnswer + 
-			"</span></p>" + 
-			questions[questionCounter].image);
+			correctAnswer + "</span></p>" + questions[questionCounter].image);
 		setTimeout(nextQuestion, 4000);
 		questionCounter++;
 	}
@@ -153,9 +76,7 @@ $(document).ready(function() {
 			incorrectGuesses++;
 			var correctAnswer = questions[questionCounter].correctAnswer;
 			$("#gameScreen").append("<p>The answer was <span class='answer'>" + 
-				correctAnswer + 
-				"</span></p>" + 
-				questions[questionCounter].image);
+				correctAnswer + "</span></p>" + questions[questionCounter].image);
 			setTimeout(nextQuestion, 4000);
 			questionCounter++;
 		}
